ad5cc77be078429666222f3e7b759681c20f06b3
japhethh <boknoy32130@gmail.com>
2025-04-05 09:41:01 +0800

    fix: Update CORS origin to use production URL for server and middleware

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://hr3.nodadogenhospital.com/",
   },
 });
 // const io = new Server(server, {
@@ -29,7 +29,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://hr3.nodadogenhospital.com",
     credentials: true,
   })
 );
