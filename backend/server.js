032450e2140c4a71e7e1432bbeb756850650fdb0
japhethh <boknoy32130@gmail.com>
2025-05-22 02:33:34 +0800

    fix: Update CORS configuration to use the correct origin for the server

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,13 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://hospital-hr3.onrender.com",
+    origin: "https://hr3.nodadogenhospital.com",
   },
 });
 
 app.use(
   cors({
-    origin: "https://hospital-hr3.onrender.com",
+    origin: "https://hr3.nodadogenhospital.com",
     credentials: true,
   })
 );
